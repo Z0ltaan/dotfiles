6e112a3aba243c07a01c330cbffbf781665656c2
Vladislav Isaychev <gregnov69@gmail.com>
2025-08-31 17:07:06 +0300

    initial

---
+++ nvim/lua/plugins/mdrenderer_config.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    ft = 'markdown',
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+      heading = {
+        -- icons = { '󰲡  ', '󰲣  ', '󰲥  ', '󰲧  ', '󰲩  ', '󰲫  ' },
+        border = true,
+        -- border_virtual = true,
+      },
+      bullet = {
+        -- icons = { '● ', '○ ', '◆ ', '◇ ' },
+      },
+      pipe_table = {
+        -- border_enabled = true,
+        -- border_virtual = true,
+        cell = 'padded',
+        filler = '',
+        -- alignment_indicator = '─',
+        style = 'full',
+      },
+      indent = {
+        enabled = true,
+        icon = '',
+      },
+      latex = { enabled = false },
+    },
+  },
+}
